7800267c1af039ab278a95f5d8250d10bf35e8e6
phugnguyen <phugnguyen@gmail.com>
2020-11-03 19:41:20 -0800

    Add jest

--- notableRegexParser.test.ts
+++ notableRegexParser.test.ts
@@ -1,5 +1,12 @@
 import { transcribeText } from './notableRegexParser';
 
 describe('notableRegexParser', () => {
-  
+  it('parses text starting the Number one', () => {
+    const testPhrase: string = "Patient presents today with several issues. Number one BMI has increased by 10% since their\
+  last visit.Number next patient reports experiencing dizziness several times in the last two\
+  weeks.Number next patient has a persistent cough that hasn’t improved for last four weeks.";
+
+    const expectedResult: string = "";
+    expect(transcribeText(testPhrase)).toEqual(expectedResult);
+  })
 })
