fa1213d41cbf1d40ae0d0dd3a8f2ed6283ce7ed8
phugnguyen <phugnguyen@gmail.com>
2020-11-03 19:58:20 -0800

    Refactor

--- notableRegexParser.test.ts
+++ notableRegexParser.test.ts
@@ -2,9 +2,10 @@ import { transcribeText } from './notableRegexParser';
 
 describe('notableRegexParser', () => {
   it('parses text starting the Number one', () => {
-    const testPhrase: string = "Patient presents today with several issues. Number one BMI has increased by 10% since their\
+    const start: string = "one";
+    const testPhrase: string = `Patient presents today with several issues. Number ${start} BMI has increased by 10% since their\
   last visit.Number next patient reports experiencing dizziness several times in the last two\
-  weeks.Number next patient has a persistent cough that hasn’t improved for last four weeks.";
+  weeks.Number next patient has a persistent cough that hasn’t improved for last four weeks.`;
 
     const expectedResult: string = "Patient presents today with several issues. \
 \n1. BMI has increased by 10% since their  last visit.\
@@ -14,9 +15,10 @@ describe('notableRegexParser', () => {
   })
 
   it('parses text starting the Number five', () => {
-    const testPhrase: string = "Patient presents today with several issues. Number five BMI has increased by 10% since their\
+    const start: string = "five";
+    const testPhrase: string = `Patient presents today with several issues. Number ${start} BMI has increased by 10% since their\
   last visit.Number next patient reports experiencing dizziness several times in the last two\
-  weeks.Number next patient has a persistent cough that hasn’t improved for last four weeks.";
+  weeks.Number next patient has a persistent cough that hasn’t improved for last four weeks.`;
 
     const expectedResult: string = "Patient presents today with several issues. \
 \n5. BMI has increased by 10% since their  last visit.\
@@ -26,9 +28,10 @@ describe('notableRegexParser', () => {
   })
 
   it('throws error when starting with number after 9', () => {
-    const testPhrase: string = "Patient presents today with several issues. Number ten BMI has increased by 10% since their\
+    const start: string = "ten";
+    const testPhrase: string = `Patient presents today with several issues. Number ${start} BMI has increased by 10% since their\
   last visit.Number next patient reports experiencing dizziness several times in the last two\
-  weeks.Number next patient has a persistent cough that hasn’t improved for last four weeks.";
+  weeks.Number next patient has a persistent cough that hasn’t improved for last four weeks.`;
 
     expect(() => transcribeText(testPhrase)).toThrow("Please use a number from one through nine and then say next. We do not currently support saying numbers after nine.")
   })
